0ab947a1ab0b1241d03ac0b3518d2b03a56e54da
aleksma <aleksma99@yandex.ru>
2019-09-16 12:06:43 +0300

    [ADD] options functional

--- homework-1/sort-task/sort.go
+++ homework-1/sort-task/sort.go
@@ -1,13 +1,20 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
-	"os"
+	"log"
+	"math"
 	"sort"
-
-	//"sort"
+	"strconv"
 	"strings"
+)
+
+const (
+	notNumericType = iota
+	intType
+	floatType
 )
 
 type Line struct {
@@ -19,51 +26,135 @@ func (l Line) String() string {
 	return fmt.Sprintf("%s", l.PureLine)
 }
 
-func WordsLess(x []string, y []string) bool {
+func WordsLess(x []string, y []string, numericFlag bool) bool {
 	if len(x) == 0 {
 		return true
 	} else if len(y) == 0 {
 		return false
 	} else if x[0] == y[0] {
-		return WordsLess(x[1:], y[1:])
-	} else {
-		return x[0] < y[0]
+		return WordsLess(x[1:], y[1:], numericFlag)
+	} else if x[0] == "" {
+		return true
+	} else if y[0] == "" {
+		return false
+	} else if numericFlag {
+		var num1, num2 float64
+		status1, status2 := notNumericType, notNumericType
+
+		if tempNum, err := strconv.ParseFloat(x[0], 64); err == nil {
+			num1 = tempNum
+			status1 = floatType
+		} else if tempNum, err := strconv.Atoi(x[0]); err == nil {
+			num1 = float64(tempNum)
+			status1 = intType
+		}
+
+		if tempNum, err := strconv.ParseFloat(y[0], 64); err == nil {
+			num2 = tempNum
+			status2 = floatType
+		} else if tempNum, err := strconv.Atoi(y[0]); err == nil {
+			num2 = float64(tempNum)
+			status2 = intType
+		}
+
+		switch {
+		case status1 != notNumericType && status2 != notNumericType:
+			return num1 < num2
+		case status1 != notNumericType:
+			return false
+		case status2 != notNumericType:
+			return true
+		}
 	}
-}
-
-type ByWords []Line
-
-func (ls ByWords) Len() int      { return len(ls) }
-func (ls ByWords) Swap(i, j int) { ls[i], ls[j] = ls[j], ls[i] }
-func (ls ByWords) Less(i, j int) bool {
-	return WordsLess(ls[i].Words, ls[j].Words)
+	return x[0] < y[0]
 }
 
 func main() {
-	if len(os.Args) < 2 {
-		fmt.Println("Usage: \"go run sort.go filename\"")
+	keyFlag := flag.Int("k", 1, "an int")
+	ignoreCaseFlag := flag.Bool("f", false, "a bool")
+	uniqueFlag := flag.Bool("u", false, "a bool")
+	reverseFlag := flag.Bool("r", false, "a bool")
+	outputFlag := flag.String("o", "", "a string")
+	numericSortFlag := flag.Bool("n", false, "a bool")
+
+	flag.Parse()
+
+	if len(flag.Args()) < 1 {
+		fmt.Println("Usage: \"go run sort.go [-k int] [-f] [-u] [-r] [-o filename] [-n] filename\"")
 		return
 	}
+	path := flag.Args()[0]
 
-	// map[string]bool
-
-	path := os.Args[1]
+	uniqueSet := make(map[string]bool)
 
 	data, _ := ioutil.ReadFile(path)
 	linesBuf := strings.Split(string(data), "\n")
-	lines := make([]Line, len(linesBuf))
+	lines := make([]Line, 0)
 
-	for i, line := range linesBuf {
+	for _, line := range linesBuf {
+
+		if *uniqueFlag {
+			uniqueLine := line
+			if *ignoreCaseFlag {
+				uniqueLine = strings.ToLower(uniqueLine)
+			}
+			if uniqueSet[uniqueLine] {
+				continue
+			} else {
+				uniqueSet[uniqueLine] = true
+			}
+		}
+
+		j := len(lines)
+		lines = append(lines, Line{})
+
 		words := strings.Split(line, " ")
-		lines[i].Words = make([]string, len(words), len(words))
-		copy(lines[i].Words, words)
-		lines[i].PureLine = line
+		if *ignoreCaseFlag {
+			words = strings.Split(strings.ToLower(line), " ")
+		}
+		wordsLen := len(words)
+		minKey := int(math.Min(float64(*keyFlag-1), float64(wordsLen)))
+		wordsBase := words[minKey:]
+		wordsTail := words[:minKey]
+		lines[j].Words = make([]string, len(wordsBase), len(wordsBase))
+		copy(lines[j].Words, wordsBase)
+		lines[j].Words = append(lines[j].Words, wordsTail...)
+		lines[j].PureLine = line
 	}
 
-	sort.Sort(ByWords(lines))
-
-	for _, line := range lines {
-		fmt.Printf("%s\n", line.PureLine)
+	if *reverseFlag {
+		sort.Slice(lines, func(i, j int) bool {
+			return !WordsLess(lines[i].Words, lines[j].Words, *numericSortFlag)
+		})
+	} else {
+		sort.Slice(lines, func(i, j int) bool {
+			return WordsLess(lines[i].Words, lines[j].Words, *numericSortFlag)
+		})
 	}
 
+	var result strings.Builder
+
+	for i, line := range lines {
+		result.WriteString(line.PureLine)
+		if i != len(lines)-1 {
+			result.WriteString("\n")
+		}
+	}
+
+	if *outputFlag == "" {
+		fmt.Println(result.String())
+	} else {
+		err := ioutil.WriteFile(*outputFlag, []byte(result.String()), 0644)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	/*	fmt.Println(uniqueSet)
+
+		for _, line := range lines {
+			fmt.Printf("%v %v\n", line.Words, len(line.Words))
+		}*/
+
+	//fmt.Println(*keyFlag, *ignoreCaseFlag, *uniqueFlag, *reverseFlag, *outputFlag, *numericSortFlag)
 }
